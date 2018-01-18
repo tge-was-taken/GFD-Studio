9a6fcdcc7d62e5bc1b2d98d96ac1f27cc68b56e6
TGEnigma <smartychrisje@hotmail.com>
2018-01-18 19:51:31 +0100

    Core/Animations: Split keyframe type 12 & 29 classes

--- AtlusGfdLib/Animations/AnimationMaterialKeyframeType12.cs
+++ AtlusGfdLib/Animations/AnimationMaterialKeyframeType12.cs
@@ -6,11 +6,4 @@
 
         public float Field00 { get; set; }
     }
-
-    public struct AnimationMaterialKeyframeType29 : IAnimationKeyframe
-    {
-        public AnimationKeyframeType KeyframeType => AnimationKeyframeType.Type29;
-
-        public float Field00 { get; set; }
-    }
 }
---
+++ AtlusGfdLib/Animations/AnimationMaterialKeyframeType29.cs
@@ -0,0 +1,9 @@
+namespace AtlusGfdLib
+{
+    public struct AnimationMaterialKeyframeType29 : IAnimationKeyframe
+    {
+        public AnimationKeyframeType KeyframeType => AnimationKeyframeType.Type29;
+
+        public float Field00 { get; set; }
+    }
+}
